7f4da152b4207973cfae72ae412a54007d200019
tridean <tridean97@gmail.com>
2023-09-06 22:17:08 -0500

    models

---
+++ models/reactions.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+},
+reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+},
+username: {
+    type: String,
+    required: true,
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleDateString(),
+},
+});
+
+module.exports = reactionSchema;
---
+++ models/thought.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new mongoose.Schema({
+thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleDateString(),
+},
+username: {
+    type: String,
+    required: true,
+},
+  reactions: [reactionSchema], // Embed the Reaction schema as an array of subdocuments
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
